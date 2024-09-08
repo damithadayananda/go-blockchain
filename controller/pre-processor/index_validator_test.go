e91a9cedcd98bcc78a50863ad520bdd774c32c83
Damitha Dayananda <damitha.dayananda@circles.asia>
2024-09-08 14:43:51 +0530

    08/09/2024 finishing block validation and get blocks by number end point

---
+++ controller/pre-processor/index_validator_test.go
@@ -0,0 +1,32 @@
+package pre_processor
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestIndexValidator_ProcessBlock(t *testing.T) {
+	type args struct {
+		block *coreBlock.Block
+	}
+	tests := []struct {
+		name  string
+		args  args
+		want  error
+		want1 *[]coreBlock.Block
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			iv := &IndexValidator{}
+			got, got1 := iv.ProcessBlock(tt.args.block)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("ProcessBlock() got = %v, want %v", got, tt.want)
+			}
+			if !reflect.DeepEqual(got1, tt.want1) {
+				t.Errorf("ProcessBlock() got1 = %v, want %v", got1, tt.want1)
+			}
+		})
+	}
+}
--- core/block/data.go
+++
@@ -1,5 +0,0 @@
-package block
-
-type DataInterface interface {
-	ToString() string
-}
